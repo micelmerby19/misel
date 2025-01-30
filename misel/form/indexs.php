6bd93696014e86839121bf9109c6ba8c5880cfbf
micelmerby19 <mayachan334@gmail.com>
2025-01-30 15:01:45 +0700

    tes rek

---
+++ misel/form/indexs.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form biodata</title>
+    <body>
+        <form action="form4.html" method="post">
+      <fieldset>
+        <legend><h1>formulir peserta <h1> </legend>
+        <p>
+<label>nisn :</label>
+<input type="text" name="Nisn" placeholder="masukan nisn">
+</p>
+<p>
+            <label>nama :</label>
+            <input type="text" name="nama" placeholder="masukan nama lengkap">
+</p>
+<p>
+    <label>tempat lahir :</label>
+    <input type="text" name="tempat lahir" placeholder="masukan tempat lahir anda">
+</p>
+<p>
+    <label>tanggal lahir:</label>
+    <input type="text" name="tanggal lahir" placeholder="masukan tanggal lahir">
+</p>
+<p>
+    <label>gender :</label>
+    <input type="radio" name="jenkel" value="laki-laki">laki-laki
+    <input type="radio" name="jenkel" value="pereempuan">perempuan
+</p>
+<p>
+    <label>agama :</label>
+    <select name="agama">
+        <option value="budha">Budha</option>
+        <option value="hindu">Hindu</option>
+        <option value="islam">Islam</option>
+        <option value="katholik">Kristen Katholik</option>
+        <option value="protestan">Kristen Protestan</option>
+</select>
+</p>
+<p>
+    <p>alamat :</p>
+    <textarea name="alamat"cols="50" rows="10"></textarea>
+</p>
+<p>
+    <label>asal sekolah :</label>
+    <input type="text" name="asal sekolah" placeholder="asal sekolah">
+</p>
+<p>
+    <label>pilih jurusan 1:</label>
+    <select name="pilih jurusan 1">
+        <option value="tjkt">teknik jaringan komputer dan telekomunikasi</option>
+        <option value="pplg">pengembangan perangkat lunak dan gim</option>
+        <option value="bdp">broadcasting dan perfilman</option>
+        <option value="pm">pemasaran</option>
+        <option value="akl">akuntansi keuangan lembaga</option>
+        <option value="mplb">manajemen perkantoran layanan bisnis</option>
+        <option value="ph">perhotelan</option>
+        <option value="kl">kuliner</option>
+        <option value="dkv">desain komunikasi visual</option>
+        <option value="sp">seni pertunjukan</option>
+</select>
+</p>
+<p>
+<label>pilih jurusan 2:</label>
+    <select name="pilih jurusan 1">
+        <option value="tjkt">teknik jaringan komputer dan telekomunikasi</option>
+        <option value="pplg">pengembangan perangkat lunak dan gim</option>
+        <option value="bdp">broadcasting dan perfilman</option>
+        <option value="pm">pemasaran</option>
+        <option value="akl">akuntansi keuangan lembaga</option>
+        <option value="mplb">manajemen perkantoran layanan bisnis</option>
+        <option value="ph">perhotelan</option>
+        <option value="kl">kuliner</option>
+        <option value="dkv">desain komunikasi visual</option>
+        <option value="sp">seni pertunjukan</option>
+</select>
+</p>
+<p>
+<label>pilih jurusan 3:</label>
+    <select name="pilih jurusan 1">
+        <option value="tjkt">teknik jaringan komputer dan telekomunikasi</option>
+        <option value="pplg">pengembangan perangkat lunak dan gim</option>
+        <option value="bdp">broadcasting dan perfilman</option>
+        <option value="pm">pemasaran</option>
+        <option value="akl">akuntansi keuangan lembaga</option>
+        <option value="mplb">manajemen perkantoran layanan bisnis</option>
+        <option value="ph">perhotelan</option>
+        <option value="kl">kuliner</option>
+        <option value="dkv">desain komunikasi visual</option>
+        <option value="sp">seni pertunjukan</option>
+</select>
+</p>
+<p>
+ekstrakulikuler yang di minati:
+<input type="checkbox" name="tari">tari
+<input type="checkbox" name="encasa">encasa
+<input type="checkbox" name="basket">basket
+<input type="checkbox" name="voli">voli
+<input type="checkbox" name="hadrah">hadrah
+<input type="checkbox" name="teater">teater
+<input type="checkbox" name="pecinta alam">pecinta array_column
+<input type="checkbox" name="osis">osis
+<input type="checkbox" name="pramuka">pramuka
+<input type="checkbox" name="remas">
+<input type="checkbox" name="uks,pmr">uks,pmr
+<input type="checkbox" name="futsal">futsal
+<input type="checkbox" name="laaskar hijau">Laskar hijau
+<input type="checkbox" name="gerakan disiplin sekolah">gerakan disiplin sekolah
+
+<br /><br />
+<button type="submit" name="submit" value="simpan">Submit!!</button>
+
+
+</body>
+</html>
